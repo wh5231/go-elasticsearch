22518538f04cf973be7dc8583c4406f4e6e971df
wangh-ah <wangh-ah@glodon.com>
2020-03-13 11:03:07 +0800

    初始

--- querytest_test.go
+++ querytest_test.go
@@ -15,10 +15,9 @@ func TestQuery(t *testing.T)  {
 			"carriage":map[string]interface{}{"sum":map[string]string{"field":"F_Freight"}},
 		},
 	}
-	/*where,_ := client.Search("md_fin_waybill").Type("md_fin_waybill").AndWhere("test","1").AndWhere("between", "F_OrderTime", "2020-03-07T00:00:00", "2020-03-07T23:59:59").AndWhere("in", "F_FJScan_Flag", "0").OrWhere("<", "F_FJScan_Flag", "1").OrWhere("in", "F_FJScan_Flag", "2").AddAggregate("group_by_customer_name",options).Do(context.Background())
-*/
+	where,_ := client.Search("index").Type("type").AndWhere("test","1").AndWhere("between", "F_OrderTime", "2020-03-07T00:00:00", "2020-03-07T23:59:59").AndWhere("in", "F_FJScan_Flag", "0").OrWhere("<", "F_FJScan_Flag", "1").OrWhere("in", "F_FJScan_Flag", "2").AddAggregate("group_by_customer_name",options).Do(context.Background())
 
 
-	where,_ := client.Search("md_fin_waybill").Type("md_fin_waybill").AndWhere("between", "F_OrderTime", "2020-03-07T00:00:00", "2020-03-07T23:59:59").AddAggregate("group_by_customer_name",options).Do(context.Background())
+
 	fmt.Println(where)
 }
